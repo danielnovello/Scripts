84070ffb0550a96459e186b69a9312a623dcb2c3
djquazzi <djquazzi@gmail.com>
2017-02-28 12:17:09 +0200

    Update pearsonaudit.sh

--- pearsonaudit.sh
+++ pearsonaudit.sh
@@ -1,5 +1,1 @@
-me=$(whoami); hostname > ~/Desktop/"$me".txt; 
-printf '%60s\n' | tr ' ' - >> ~/Desktop/"$me".txt;
-system_profiler SPHardwareDataType  >> ~/Desktop/"$me".txt;
-mail -s "Pearson Audit $me" daniel.novello@pearson.com < ~/Desktop/$me.txt;
-rm ~/Desktop/$me.txt
+me=$(whoami); hostname > ~/Desktop/"$me".txt; printf '%60s\n' | tr ' ' - >> ~/Desktop/"$me".txt; system_profiler SPHardwareDataType  >> ~/Desktop/"$me".txt; mail -s "Pearson Audit $me" daniel.novello@pearson.com < ~/Desktop/$me.txt; rm ~/Desktop/$me.txt
