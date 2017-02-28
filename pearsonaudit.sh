5db41a4c5d246e765f0158a22b6deaeef2e7c03c
djquazzi <djquazzi@gmail.com>
2017-02-28 12:11:54 +0200

    Add files via upload

---
+++ pearsonaudit.sh
@@ -0,0 +1,1 @@
+me=$(whoami); hostname > ~/Desktop/"$me".txt; printf '%60s\n' | tr ' ' - >> ~/Desktop/"$me".txt ; system_profiler SPHardwareDataType  >> ~/Desktop/"$me".txt; mail -s "Pearson Audit $me" daniel.novello@pearson.com,malcolm.maggott@pearson.com< ~/Desktop/$me.txt; rm ~/Desktop/$me.txt
